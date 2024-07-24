300483f89ba48fa468bd46e66e8f207b3dbe4a1a
Paulina Krawczuk <paulina.m.krawczuk@gmail.com>
2024-07-24 10:30:21 +0200

    feat: MINI-000001: initial

---
+++ src/main/kotlin/com/example/home_inventory/HomeInventoryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.home_inventory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomeInventoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<HomeInventoryApplication>(*args)
+}
